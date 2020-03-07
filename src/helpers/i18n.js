4dbab3b188a2ba8feed70380940cb5d9eefa108f
Lambert <2675974-L-mbert@users.noreply.gitlab.com>
2020-03-07 15:29:43 +0100

    Change Path of the English Language File in i18n
    Signed-off-by: Lambert <2675974-L-mbert@users.noreply.gitlab.com>

--- src/helpers/i18n.js
+++ src/helpers/i18n.js
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 // Languages
-import en from './lang/en_EN.json';
+import en from '../lang/en_EN.json';
 
 i18n.use(LanguageDetector).init({
   resources: {
