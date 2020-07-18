37f78f72804d671f064bd1ce2a220a749405831f
necm1 <6142251-necm1@users.noreply.gitlab.com>
2020-07-18 19:44:07 +0200

    added flex to login container

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,6 +5,8 @@ import { UserService } from 'src/app/services/user.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import {AlertService} from '../../services/alert.service';
+import {HttpErrorResponse} from '@angular/common/http';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
@@ -17,12 +19,14 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   authSubscription: Subscription;
   userSubscription: Subscription;
+  translationSubscription: Subscription;
 
   constructor(private titleService: TitleService,
               private formBuilder: FormBuilder,
               private userService: UserService,
               private router: Router,
-              private alertService: AlertService) { }
+              private alertService: AlertService,
+              private translateService: TranslateService) { }
 
   ngOnInit(): void {
     this.authForm = this.formBuilder.group({
@@ -30,22 +34,29 @@ export class HomeComponent implements OnInit, OnDestroy {
       password: ['', Validators.required]
     });
 
-    this.alertService.error("test");
-
     this.titleService.setTitle('Home');
   }
 
   onSubmit(): void {
     this.isSubmited = true;
 
+    if (!this.f.username.value || !this.f.password.value) {
+      this.translationSubscription = this.translateService.get('HOME.FORM.INPUT.EMPTY').subscribe({
+        next: (value: string) => this.alertService.error(value),
+        error: () => this.alertService.error('Please enter your username and password')
+      });
+
+      return;
+    }
+
     this.authSubscription = this.userService.auth(this.f.username.value, this.f.password.value).subscribe({
       next: (e) => {
         this.userSubscription = this.userService.getUser().subscribe({
           next: () => this.router.navigateByUrl('/dashboard'),
-          error: e => console.log(e)
+          error: (e: HttpErrorResponse) => this.alertService.error(e.error.message)
         });
       },
-      error: (e) => console.log(e)
+      error: (e: HttpErrorResponse) => this.alertService.error(e.error.message)
     });
   }
 
@@ -56,6 +67,10 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     if (this.userSubscription && !this.userSubscription.closed) {
       this.userSubscription.unsubscribe();
+    }
+
+    if (this.translationSubscription && !this.translationSubscription) {
+      this.translationSubscription.unsubscribe();
     }
   }
 
