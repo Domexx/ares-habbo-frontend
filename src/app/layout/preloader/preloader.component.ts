e7ba42ca24e0dbd4a72cd4e798abf535584d008b
necmi <necmi.m.hasan@outlook.de>
2020-08-02 23:31:51 +0200

    added article template, friends, room, footer modal and preloader

---
+++ src/app/layout/preloader/preloader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ares-preloader',
+  templateUrl: './preloader.component.html',
+  styleUrls: ['./preloader.component.scss']
+})
+export class PreloaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
