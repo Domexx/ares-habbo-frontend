674f37c8c75be23e64185dbfaf04adbf31152abf
necmi <necmi.m.hasan@outlook.de>
2020-08-10 18:21:17 +0200

    added example.environment and removed file replacement in angular.json

---
+++ src/environments/example.environment.ts
@@ -0,0 +1,21 @@
+import 'zone.js/dist/zone-error';
+
+// Don't forget to rename `example.environment.ts` to environment.ts
+
+export const environment = {
+  production: true,
+
+  app: {
+    // API
+    endpoint: 'https://yourdomain.com/api',
+
+    // Website
+    hotelName: 'Ares',
+    title: 'Ares ~ ',
+    imager: 'https://www.habbo.com/habbo-imaging/avatarimage?figure=',
+
+    // Language
+    defaultLang: 'en',
+    useLang: 'de'
+  },
+};
