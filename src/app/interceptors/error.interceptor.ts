d0a4964573c634c2d20fd7cc9080325de5bb1b08
necmi <necmi.m.hasan@outlook.de>
2020-09-09 14:47:38 +0200

    fixed logout

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse} from '@angular/common/http';
-import {Observable, throwError} from 'rxjs';
+import {Observable, of, throwError} from 'rxjs';
 import {catchError} from 'rxjs/operators';
 import {UserService} from 'src/app/services/user.service';
 import {AlertService} from '../services/alert.service';
@@ -9,33 +9,33 @@ import {TranslateService} from '@ngx-translate/core';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+  isLoggingOut = false;
+
   constructor(
     private userService: UserService,
     private alertService: AlertService,
     private translateService: TranslateService,
     private router: Router
-  ) {
-  }
+  ) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((err: HttpErrorResponse | any) => {
-        if (err.status === 401 && (this.userService.user || this.userService.token)) {
+        if (!this.isLoggingOut && err.status === 401 && (this.userService.user || this.userService.token)) {
+          this.isLoggingOut = true;
+
           this.userService.logout().finally(() => this.router.navigateByUrl('/')
-            .then(() => this.alertService.error(this.translateService.instant('LOGOUT.ERROR'))));
-
-          return;
+            .then(() => {
+              this.alertService.error(this.translateService.instant('LOGOUT.ERROR'));
+              this.isLoggingOut = false;
+            }));
         }
 
-        if (err.status === 404) {
-          return throwError(err);
-        }
-
-        if (err instanceof HttpErrorResponse && err.error.errors) {
+        if (!this.isLoggingOut && err instanceof HttpErrorResponse && err.error.errors) {
           err.error.errors.forEach(key => {
             this.alertService.error(key.message);
           });
-        } else if (!err.error) {
+        } else if (!this.isLoggingOut && !err.error) {
           this.alertService.error(this.translateService.instant('UNKNOWN_ERROR'));
         }
 
