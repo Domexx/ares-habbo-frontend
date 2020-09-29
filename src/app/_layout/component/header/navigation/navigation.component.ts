b66721dd4d6edba1b6838637a545ef1d41cc4d2a
necm1 <6142251-necm1@users.noreply.gitlab.com>
2020-09-29 17:43:43 +0200

    fixed navigation - parent route active in child route

--- src/app/_layout/component/header/navigation/navigation.component.ts
+++ src/app/_layout/component/header/navigation/navigation.component.ts
@@ -1,13 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { environment } from 'src/environments/environment';
 import {UserService} from '../../../../_shared/service/user.service';
+import {ActivatedRoute, NavigationStart, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'ares-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnInit, OnDestroy {
   username: string;
   look: string;
 
@@ -16,8 +18,12 @@ export class NavigationComponent implements OnInit {
 
   isCollapsed = false;
 
+  route: string;
+  routerSubscription: Subscription;
+
   constructor(
     private userService: UserService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -26,10 +32,22 @@ export class NavigationComponent implements OnInit {
 
     this.username = this.userService.user.username;
     this.look = this.userService.user.look;
+
+    this.routerSubscription = this.router.events.subscribe(value => {
+      if (value instanceof NavigationStart) {
+        this.route = value.url;
+      }
+    });
   }
 
   toggle() {
     this.isCollapsed = !this.isCollapsed;
+  }
+
+  ngOnDestroy() {
+    if (this.routerSubscription && !this.routerSubscription.unsubscribe) {
+      this.routerSubscription.unsubscribe();
+    }
   }
 
 }
