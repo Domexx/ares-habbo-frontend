796aba3512c262f742866107ce2eebf0ce588951
necm1 <necmi.m.hasan@outlook.de>
2020-11-09 04:22:22 +0100

    [TASK] added global settings to sidebar

---
+++ src/app/hobba/hobba.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HobbaRoutingModule } from './hobba-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HobbaRoutingModule
+  ]
+})
+export class HobbaModule { }
